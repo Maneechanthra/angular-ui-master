444d205f379d92a8c4342dc76e6bd3b887ab8f56
sumet <sumet.ma@ku.th>
2024-08-30 23:41:21 +0700

    first commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,12 @@
+import { Routes } from '@angular/router';
+import { IndexComponent } from './index/index.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { BoardComponent } from './board/board.component';
+
+export const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'index', component: IndexComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'board/:id', component: BoardComponent },
+];
